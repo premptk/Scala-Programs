bb6c6d1d7265e441fa22e11157053cc6d7d337a2
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-08-02 12:14:53 +0530

    Function which returns sum of two input integers
    Function which returns sum of two input integers.

---
+++ Basic/addTwoNum.scala
@@ -0,0 +1,13 @@
+
+
+object addTwoNum {
+  def main(args : Array[String])
+  {
+    println("Result : " + add(10,20))
+  }
+  
+  def add(x:Int, y:Int) : Int = {
+    val sum = x+y
+    return sum
+  }
+}
