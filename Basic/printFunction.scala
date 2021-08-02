e8452ae5f8bb07366f770f61db050aedd76224f2
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-08-02 12:13:01 +0530

    print statement in Scala
    Scala supports 4 types of print statements, each similar to python, C, C++, and Java.

---
+++ Basic/printFunction.scala
@@ -0,0 +1,10 @@
+
+
+object printFunction {
+  def main(args: Array[String])={
+    Console.println("We are graphians!")
+    print("Studying in CSE\n")
+    println("Section - DS")
+    printf("Age = %d",24)
+  }
+}
