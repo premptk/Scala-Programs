81025d85b2cdae7c7acf1b154dc86fcd014d81dc
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-11-13 23:55:19 +0530

    Create Ques12.scala

---
+++ End-Term Assignments/Ques12.scala
@@ -0,0 +1,21 @@
+import org.ddahl.rscala._
+import ru.retailrocket.ispark._
+object DataScience{
+    def connect = RClient("R", false)
+    @transient
+    val r = connect()
+    def dataViz():Unit={
+        R.plot(r.c("x", 1 to 10), r.c("y", 1 to 10)) // Random data to visualize
+    }
+    def dataDescription(data: DataFrame): Unit = {
+        val data_desc = data.describe() // Summarize the data usnig inbuilt functions
+        data_desc.show()
+    }
+    def main(args: Array[String]): Unit = {
+        println("Name : Prem Ranjan Pattanayak")
+        println("Section : DS         ")
+        println("Roll No : 47         ")
+        dataViz()
+        dataDescription("himanshu/Desktop/sample.csv")
+    }
+}
