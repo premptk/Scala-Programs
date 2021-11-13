92acd62cc84431d275d24621294e942446992dee
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-11-13 23:50:45 +0530

    Create Ques6.scala

---
+++ End-Term Assignments/Ques6.scala
@@ -0,0 +1,41 @@
+object method_implementations {
+  def main(args: Array[String]): Unit = {
+    println("Name : Prem Ranjan Pattanayak")
+    println("Section : DS         ")
+    println("Roll No : 47         ")
+    var list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    println(removeDuplicates(list));
+    println(find_ele(list, 5));
+    println(listLength(list));
+    println(sort(list));
+    println(sum(list));
+    println(listToString(list));
+    println(smallest(list));
+    println(maximum(list));
+  }
+  def removeDuplicates(list: List[Int]): Unit = {
+    var newList = list.distinct;
+    println(newList);
+  }
+  def find_ele(list: List[Int], ele: Int): Boolean = {
+    return list.contains(ele);
+  }
+  def listLength(list: List[Int]): Int = {
+    return list.length;
+  }
+  def sort(list: List[Int]): List[Int] = {
+    return list.sorted;
+  }
+  def sum(list: List[Int]): Int = {
+    return list.sum;
+  }
+  def listToString(list: List[Int]): String = {
+    return list.toString();
+  }
+  def smallest(list: List[Int]): Int = {
+    return list.min;
+  }
+  def maximum(list: List[Int]): Int = {
+    return list.max;
+  }
+}
