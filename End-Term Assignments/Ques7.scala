0faae5ed46af38b59a0d61cedc81c0c973601732
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-11-13 23:52:15 +0530

    Create Ques7.scala

---
+++ End-Term Assignments/Ques7.scala
@@ -0,0 +1,16 @@
+object max_of_both{
+    def main(args:Array[String]):Unit={
+        println("Name : Prem Ranjan Pattanayak")
+        println("Section : DS         ")
+        println("Roll No : 47         ")
+        var list1 = List(99.5, 100, 50, 55, 70, 100, -1)
+        var list2 = List(10, 20, 30, 40, 50)
+        print("Concatenated : ")
+        println(List.concat(list1, list2))
+        print("Max : ")
+        println(list1.max)
+        print("Min : ")
+        println(list1.min)
+
+    }
+}
