4a10c3f4aa0c1150f3813817cff6864fe1428adc
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-11-13 23:48:48 +0530

    Create Ques4.scala

---
+++ End-Term Assignments/Ques4.scala
@@ -0,0 +1,30 @@
+import scala.annotation.tailrec
+object tailrec{
+    def main(args: Array[String]): Unit = {
+         println("Name : Prem Ranjan Pattanayak")
+        println("Section : DS         ")
+        println("Roll No : 47         ")
+        println("Enter two numbers")
+        var a = scala.io.StdIn.readInt()
+        var b = scala.io.StdIn.readInt()
+        print(s"Factorial of $a = ")
+        println(factorial(a))
+        print(s"GCD of $a, $b = ")
+        println(gcd(a, b))
+    }
+    
+    def factorial(n: Int): Int = {
+        @tailrec
+        def factorialAcc(acc: Int, n: Int): Int = {
+        if (n <= 1) acc
+        else factorialAcc(acc * n, n - 1)
+        }
+        factorialAcc(1, n)
+    }
+
+    @tailrec
+    def gcd(a: Int, b: Int): Int = {
+        if (b == 0) a
+        else gcd(b, a % b)
+    }
+}
