7a69940568090d5cddc5bbcc412dde70b1555dca
Prem Ranjan Pattanayak <premranjan210@gmail.com>
2021-11-13 23:49:33 +0530

    Create Ques5.scala

---
+++ End-Term Assignments/Ques5.scala
@@ -0,0 +1,20 @@
+// demonstrate all builtin operation on lists
+object lists{
+    def main(args:Array[String]):Unit={
+        println("Name : Prem Ranjan Pattanayak")
+        println("Section : DS         ")
+        println("Roll No : 47         ")
+        val list = List(1,2,3,4,5)
+        println("list = "+list)
+        println("head = "+list.head)
+        println("tail = "+list.tail)
+        println("Empty or not = "+list.isEmpty)
+        println("Length = "+list.length)
+        println("Reverse = "+list.reverse)
+        println("Take first few = "+list.take(3))
+        println("Drop first few = "+list.drop(3))
+        println("Split list = "+list.splitAt(3))
+        println("Range of indices = "+list.indices)
+        println("String of list = "+list.indices.map(list(_)).mkString(","))
+    }
+}
